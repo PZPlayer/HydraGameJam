9a63f72a728fe69597078b3cf2f17b5a7d749615
В3441_МагераМА <1arya11stark@gmail.com>
2025-02-16 15:38:50 +0300

    В разработке 3 уровень.

--- Assets/Scripts/MarinasFolder/ButtonsDown/TriggerBottonDown.cs
+++ Assets/Scripts/MarinasFolder/ButtonsDown/TriggerBottonDown.cs
@@ -3,10 +3,25 @@ using UnityEngine;
 public class TriggerBottonDown : MonoBehaviour
 {
     private bool isDropping = false;
+    [SerializeField] private bool canPlayer = true;
+    [SerializeField] private float scale = 1;
     private void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("BoxActive") || other.CompareTag("Player"))
+        if (other.CompareTag("BoxActive"))
         {
+            // Получаем Collider объекта
+            Collider boxCollider = other.GetComponent<Collider>();
+            if (boxCollider != null)
+            {
+                Vector3 boxSize = boxCollider.bounds.size;
+                if (boxSize.x == scale) // Замените 1.0 на подходящее значение для вашего случая
+                {
+                    isDropping = true;
+                }
+            }
+        }
+        if (other.CompareTag("Player") && canPlayer)
+        {            
             isDropping = true;
         }
     }
